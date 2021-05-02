b121af46e6e33732d5c9d417e1721fae9417df08
Vishal Pillai <68294182+VishalPillai-2020@users.noreply.github.com>
2021-05-02 14:30:13 +0530

    Update main.js

--- main.js
+++ main.js
@@ -34,10 +34,36 @@ function getPoses(results) {
 
 function draw() {
     background("grey");
-     
-    document.getElementById("shape_name").innerHTML = "Size of the shape is"+ difference+"px";
+    shape = document.getElementById("select").value;
+    document.getElementById("shape_name").innerHTML = "Size of the " + shape + " is" + difference + "px";
     stroke("black");
     fill("white");
-    square( noseX, noseY, difference);
-   
+    if (shape == "Square") {
+        square(noseX, noseY, difference);
+    };
+    if (shape == "Circle") {
+        circle(noseX, noseY, difference);
+    }
+    if (shape == "Rectangle") {
+        Breadth = difference * 0.66;
+        rect(noseX, noseY, difference, Breadth);
+
+    }
+    if (shape == "Triangle") {
+        difference_1 = difference / 2;
+        X1 = noseX - difference_1;
+        X3 = noseX + difference_1;
+        y = noseY + difference_1;
+        triangle(X1, y, noseX, noseY, X, y);
+    }
+    if(shape=="Flower"){
+        noStroke();
+        translate(noseX, noseY);
+        for(var i=0; i < 10; i++){
+            ellipse(0,20,difference,20);
+            rotate(PI / 5);
+        }
+    }
+
+
 }
